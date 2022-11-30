1f3788dff9044d84132f28714fb0c0d6c8f7c64d
Matt Hummer <biggreyson@gmail.com>
2022-11-30 09:22:31 -0700

    move some stuff around and manage port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs      = require('fs');
 
 const app = express();
-const port = 6969;
+const port = process.env.PORT || 6969;
 
 app.use(express.static('public'));
 
